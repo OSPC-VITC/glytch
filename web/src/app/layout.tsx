a29802fec179b732dd4890ea85c4af5563df9e01
Nandu Mahesh <nandumahesh2004@gmail.com>
2025-09-14 16:35:14 +0530

    fixed background and added transitions from hero to the next setions

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Header } from "@/components/layout/Header";
+import Galaxy from "@/components/ui/galaxy";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +30,9 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Header />
+        <div className="fixed inset-0 z-0" aria-hidden>
+          <Galaxy transparent />
+        </div>
         {children}
       </body>
     </html>
